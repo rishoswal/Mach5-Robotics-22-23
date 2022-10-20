491ca534a23382bdbc3e96a58c6eecd3438b9259
Alan J <alanjiang170@gmail.com>
2022-10-19 19:31:27 -0700

    add stuff test
    added v5code folder to see if stuff works

---
+++ SpinUp1/src/main.cpp
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\catsc                                            */
+/*    Created:      Wed Jul 13 2022                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// driveBR              motor         4               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+
+  //mecanum drive
+  while(true){
+    y
+  }
+}
