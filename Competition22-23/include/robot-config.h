1eb1e5a6f3c82eef8efea847289ae668e55025c5
Anshu Laptop <80088094+rishoswal@users.noreply.github.com>
2022-12-10 16:32:01 -0800

    dont use
    it crashed

--- Competition22-23/include/robot-config.h
+++ Competition22-23/include/robot-config.h
@@ -5,16 +5,17 @@ extern brain Brain;
 // VEXcode devices
 extern controller Controller1;
 extern motor Flywheel;
-extern motor Motor19;
-extern motor Motor20;
-extern motor Motor18;
-extern motor Motor16;
-extern motor Motor17;
-extern motor Motor12;
+extern motor left1;
+extern motor left2;
+extern motor left3;
+extern motor right1;
+extern motor right2;
+extern motor right3;
 extern digital_out DigitalOutH;
 extern digital_out DigitalOutG;
 extern motor Intake;
 extern inertial Inertial;
+extern inertial Inertial3;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
--- Competition22-23/src/robot-config.cpp
+++ Competition22-23/src/robot-config.cpp
@@ -10,16 +10,17 @@ brain  Brain;
 // VEXcode device constructors
 controller Controller1 = controller(primary);
 motor Flywheel = motor(PORT9, ratio6_1, false);
-motor Motor19 = motor(PORT19, ratio18_1, false);
-motor Motor20 = motor(PORT20, ratio18_1, false);
-motor Motor18 = motor(PORT18, ratio18_1, false);
-motor Motor16 = motor(PORT16, ratio18_1, true);
-motor Motor17 = motor(PORT17, ratio18_1, true);
-motor Motor12 = motor(PORT12, ratio18_1, true);
+motor left1 = motor(PORT19, ratio18_1, false);
+motor left2 = motor(PORT20, ratio18_1, false);
+motor left3 = motor(PORT18, ratio18_1, false);
+motor right1 = motor(PORT16, ratio18_1, true);
+motor right2 = motor(PORT17, ratio18_1, true);
+motor right3 = motor(PORT12, ratio18_1, true);
 digital_out DigitalOutH = digital_out(Brain.ThreeWirePort.H);
 digital_out DigitalOutG = digital_out(Brain.ThreeWirePort.G);
 motor Intake = motor(PORT14, ratio6_1, false);
 inertial Inertial = inertial(PORT1);
+inertial Inertial3 = inertial(PORT3);
 
 // VEXcode generated functions
 // define variable for remote controller enable/disable
